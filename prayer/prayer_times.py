0ffe2b342442f1b8f442c3e062f1f91a4f03e334
MoHaMeD <100963463+Mo7amedAtwan@users.noreply.github.com>
2022-09-20 23:22:33 +0200

    Update prayer_times.py

--- prayer/prayer_times.py
+++ prayer/prayer_times.py
@@ -1,29 +1,25 @@
-
+###IMPORTING###
+from tkinter import *
+from datetime import datetime
 import requests
 from bs4 import BeautifulSoup as S
+
+
+#API
 headers={'user-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36'}
 req=requests.get('https://timesprayer.com/en/prayer-times-in-kafr-al-sheikh.html',headers=headers)
 soup=S(req.content,'html.parser')
 obj=soup.find(class_="info prayertable mobile").find_all('li')
-
-""" from time import strftime
- """
-
  
- 
-###IMPORTING###
-from tkinter import *
-from datetime import datetime
 
-
+#make window
 test = Tk()
-
 test.title("PRAYER TIMES AT KAFR ASHYKH")
 test.geometry("450x700")
 test.minsize(450,700)
 test.maxsize(450,700)
 
-
+#current time
 now = datetime.now()
 time_now=now.strftime("%I:%M %p")
 
@@ -40,6 +36,8 @@ l8=Label(test,text=obj[4].text,height=1,font=("Arial",40),fg='#B9848C').pack()
 l9=Label(test,text=obj[5].text,height=1,font=("Arial",40),fg='#806491').pack()
 l10=Label(test,text='',height=1,font=("Arial",10)).pack()
 exit_button=Button(test,text="QUIT",height=1,font=("ِArial",20),command=test.destroy,relief="raised",bd=10,fg='#E7EAEF',bg='#37745B').pack()
+
+
 #$$$$run infintly$$$$
 test.mainloop()   
 

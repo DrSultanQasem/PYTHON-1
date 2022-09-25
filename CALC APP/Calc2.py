9b0d6a0311f9566b707f24bfe4f3bd205041d309
Mo7amedAtwan <mo7amed3twan@gmail.com>
2022-09-25 19:21:09 +0200

    #

--- CALC APP/Calc2.py
+++ CALC APP/Calc2.py
@@ -22,33 +22,40 @@ print("This from decimal to octal",fr_de_to_oc(101))
 print("This from octal to decimal",fr_oc_to_de(101))
 print("This from decimal to hexa",fr_de_to_hx(101)) 
 """
-
+##################1
 def fr_bi_to_oc(x):
     x_1=fr_bi_to_de(x)
     x_2=fr_de_to_oc(x_1)
     return(x_2)
 print("This from binary to octal",fr_bi_to_oc(101))
-
+##################2
 def fr_oc_to_bi(x):
     x_1=fr_oc_to_de(x)
     x_2=fr_de_to_bi(x_1)
     return(x_2)
 print("This from octal to binary",fr_oc_to_bi(101))
-##################
+##################3
 def fr_bi_to_hx(x):
     x_1=fr_bi_to_de(x)
     x_2=fr_de_to_hx(x_1)
     return(x_2)    
 print("This from binary to hexa",fr_bi_to_hx(101))
-#################
-    
+#################4
 def fr_oc_to_hx(x):
-    pass
-        
-
+    x_1=fr_oc_to_de(x)
+    x_2=fr_de_to_hx(x_1)
+    return(x_2)
 print(fr_oc_to_hx(101))
+
+
+
+
+
+
+
+
+
 """  
-   
 ######################################################
     def fr_hx_to_oc(self):
         return(oct(self.fr)).replace("0o","OCTAL : ")
@@ -59,18 +66,9 @@ print(fr_oc_to_hx(101))
     def fr_hx_to_de(self):
         return(int(self.fr))
     #note must enter number 0x??? & CAPITAL ELEMNT    
-###################################################### 
- 
+######################################################  
  """
-
-
 
 #print(fr_hx_to_bi())
 #print(fr_hx_to_de())
 #print(fr_hx_to_oc())
-
-
-
-
-
-

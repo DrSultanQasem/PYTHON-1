a770d8fe946b69d7aef32fb84ed4ce4d38db0f19
Mo7amedAtwan <mo7amed3twan@gmail.com>
2022-10-20 22:55:11 +0200

    1

---
+++ test.py
@@ -0,0 +1,23 @@
+stars=[
+    'sol',
+    'Alpha centauri',
+    'bramder',
+    'Wolf 359'
+    ]
+
+print   (stars[3])
+
+peaks={
+    'African':'Kilimanjaro',
+    'Antarctica':'Vinson',
+    'Australian':'Puncak Jaya',
+    'Eurasian':'Everest',
+    'North_American':'Denali',
+    'Pacific':'Mauna Kea',
+    'South_American':'Aconcagua'    
+}
+
+print   (peaks['Pacific'])
+
+
+
